635f49c8eb55d0ba394cd18706b8b7689e70daa1
ezurechan <ezurem11+github@gmail.com>
2022-07-02 19:34:43 +0900

    ユーザーのログアウト機能までやったよ

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :firstname_kana, presence: true
+  validates :lastname_kana, presence: true
+  validates :birth_date, presence: true  
+    
+end
