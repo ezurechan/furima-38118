65e42700fd0d57932b554bfb3b6d5cb1b45a91ef
ezurechan <ezurem11+github@gmail.com>
2022-07-03 19:37:30 +0900

    修正3

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,8 +4,13 @@ RSpec.describe User, type: :model do
     @user = FactoryBot.build(:user)
   end
 
-#異常系テスト
-  describe 'ユーザー新規登録' do
+describe 'ユーザー新規登録' do
+  context '新規登録できないとき' do
+    it '全ての値が存在すれば登録できる' do
+      expect(@user).to be_valid
+    end
+  end
+  context '新規登録できないとき' do
     it 'nicknameが空では登録できない' do
       @user.nickname = ''
       @user.valid?
@@ -63,9 +68,7 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include('Password は半角英数を両方含む必要があります')
     end
-  end
 
-  describe '新規登録/本人情報確認' do
     it 'お名前(全角)は、名字がないと登録できない' do
       @user.last_name = ''
       @user.valid?
@@ -118,9 +121,6 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("Birth date can't be blank")
     end
   end
-  
-  #正常系テスト
-  it '全ての値が存在すれば登録できる' do
-    expect(@user).to be_valid
-  end
+end
+
 end
