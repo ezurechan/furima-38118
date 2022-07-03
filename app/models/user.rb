bee0f7e314295e5169d48c634b2ca7479792541b
ezurechan <ezurem11+github@gmail.com>
2022-07-03 15:13:36 +0900

    ログイン画面その他

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,9 @@ class User < ApplicationRecord
   validates :last_name, presence: true
   validates :firstname_kana, presence: true
   validates :lastname_kana, presence: true
-  validates :birth_date, presence: true  
+  validates :birth_date, presence: true
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります'}
     
 end
