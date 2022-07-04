152fed8d2fe3e6ddc70582e34d769c57169d9885
ezurechan <ezurem11+github@gmail.com>
2022-07-04 17:38:26 +0900

    - [x] 手順6 出品した商品の情報をテーブルに保存できるようにする

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,20 @@
 class ItemsController < ApplicationController
   
-  def create
+  def create    
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
   
   def new
+    @item = Item.new
   end
   
   private
   def item_params
-    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :item_name, :item_info, :item_category_id, :item_sales_status_id, :item_shipping_fee_status_id, :item_prefecture_id, :item_scheduled_delivery_id, :item_price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,11 @@ class Item < ApplicationRecord
   has_many :orders
   has_one_attached :image
 
+  validates :image, presence: true
+  validates :item_name, presence: true
+  validates :item_info, presence: true
+  validates :item_price, presence: true
+
   validates :item_category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :item_prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :item_sales_status_id, numericality: { other_than: 1 , message: "can't be blank"}
