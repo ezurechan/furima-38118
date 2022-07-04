063e442e5cbd4581c5da19f774801c63417ca2a7
ezurechan <ezurem11+github@gmail.com>
2022-07-04 16:19:54 +0900

    - [x] 手順5 商品出品ページを表示できるようにする、まで

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,11 @@
 class ItemsController < ApplicationController
+  
+  def create
+  end
+  
+  def new
+  end
+  
   private
   def item_params
     params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,15 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :item_category
+
   has_many :items
   has_many :orders
   has_one_attached :image
+
+  validates :item_category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :item_prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :item_sales_status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :item_scheduled_delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :item_shipping_fee_status_id, numericality: { other_than: 1 , message: "can't be blank"}
+
 end
