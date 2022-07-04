eae44873c1d6c672e153fe742baf4b364b505c46
ezurechan <ezurem11+github@gmail.com>
2022-07-04 19:16:53 +0900

    単体テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,21 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :item_category
+  belongs_to :item_prefecture
+  belongs_to :item_sales_status
+  belongs_to :item_scheduled_delivery
+  belongs_to :item_shipping_fee_status
+  
+  belongs_to :user
 
-  has_many :items
   has_many :orders
   has_one_attached :image
 
   validates :image, presence: true
   validates :item_name, presence: true
   validates :item_info, presence: true
-  validates :item_price, presence: true
+  validates :item_price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+             format: { with: /\A[0-9]+\z/ }
 
   validates :item_category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :item_prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
