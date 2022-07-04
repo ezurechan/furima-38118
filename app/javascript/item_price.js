6a0a8cb347b5af7a292897fe57e65a6480a64342
ezurechan <ezurem11+github@gmail.com>
2022-07-04 20:14:47 +0900

    Step2 HTMLをJavaScriptで作成し、ブラウザ上に描画できるようにする

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -3,6 +3,10 @@ window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    console.log(inputValue);
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.floor(priceInput.value * 0.1)
+    const profitDom = document.getElementById("profit");
+    profitDom.innerHTML = Math.floor(priceInput.value * 1.1)
+    console.log(profitDom);
 })
 });
