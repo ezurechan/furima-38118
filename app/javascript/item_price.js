b4ce228b3c10d2d2edddc221574f068e9c829a7f
ezurechan <ezurem11+github@gmail.com>
2022-07-04 19:48:25 +0900

    Step2 必要なvalue属性を取得できるようにする

---
+++ app/javascript/item_price.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', () => {
+
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    console.log(inputValue);
+})
+});
