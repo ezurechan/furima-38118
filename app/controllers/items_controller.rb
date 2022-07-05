c4a54412e99451f23eff0654829704178b32dcc1
ezurechan <ezurem11+github@gmail.com>
2022-07-05 21:30:49 +0900

    手順1~3
    手順1 indexアクションへのルーティングを記述する
    
    コントローラー側で全ての商品情報を含んだインスタンス変数を設定する
    (PicTweetにおいて「ツイート一覧機能」を実装する際の記述です。)
    
    手順3 eachメソッドを使って、商品情報を表示する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,10 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
 
-  
+  def index
+    @items = Item.all
+  end
+
   def create    
     @item = Item.new(item_params)
     if @item.save
