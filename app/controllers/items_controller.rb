d53280dae783f6aabc4259d15246356397d94761
ezurechan <ezurem11+github@gmail.com>
2022-07-07 18:43:02 +0900

    Revert "手順6 updateアクションを定義する"
    This reverts commit 9e3f3d8d8818fa42ab436af434fb1f78848618b0.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,10 +25,6 @@ class ItemsController < ApplicationController
   def edit
     @item = Item.find(params[:id])
   end
-
-  def update
-    @item = Item.find(params[:id])
-  end
   
   private
   def item_params
