edc3bd80b1c0870d96c26f664c7fc38a31f27157
ezurechan <ezurem11+github@gmail.com>
2022-07-09 20:12:29 +0900

    手順4 フォームの情報を保存できるようにする

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,16 +6,18 @@ class OrdersController < ApplicationController
   end
 
   def create    
-    @item = Item.new(item_params)
-    if @item.save
+    @item = Item.find(params[:item_id])
+    @order_form = OrderForm.new(order_params)
+    if  @order_form.valid?
+      @order_form.save
       redirect_to root_path
     else
-      render :new
+      render :index
     end
   end
 
   private
-  def item_params
-    params.require(:item).permit(:image, :item_name, :item_info, :item_category_id, :item_sales_status_id, :item_shipping_fee_status_id, :item_prefecture_id, :item_scheduled_delivery_id, :item_price).merge(user_id: current_user.id)
+  def order_params
+    params.require(:order_form).permit(:postal_code, :item_prefecture_id, :city, :addresses, :building, :phone_number ).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,17 +1,16 @@
 class OrderForm
   include ActiveModel::Model
-  attr_accessor :item, :user, :postal_code, :item_prefecture_id, :city, :addresses, :building, :phone_number, :order
+  attr_accessor :item_id, :user_id, :postal_code, :item_prefecture_id, :city, :addresses, :building, :phone_number
   
-  validates :item, presence: true
-  validates :user, presence: true
+  validates :item_id, presence: true
+  validates :user_id, presence: true
   validates :postal_code, presence: true
   validates :item_prefecture_id, presence: true
   validates :city, presence: true
   validates :phone_number, presence: true
-  validates :order, presence: true
 
   def save
-    Order.create(item: item, user: user,)
+    order = Order.create(item_id: item_id, user_id: user_id)
     Shipment.create( postal_code: postal_code, item_prefecture_id: item_prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number, order: order)
   end
  end
