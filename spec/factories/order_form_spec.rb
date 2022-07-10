6054f6764c64dd3f23c587c01ba1aaeba2974937
ezurechan <ezurem11+github@gmail.com>
2022-07-10 19:31:44 +0900

    手順5 テストコードを編集する

--- spec/factories/order_form_spec.rb
+++ spec/factories/order_form_spec.rb
@@ -7,6 +7,7 @@ FactoryBot.define do
     addresses           { '夏山1-2-3' }
     building            { 'サマーハイツ101' }
     phone_number        { '09012345678' }
+    token               {"tok_abcdefghijk00000000000000000"}
 
   end
 end
