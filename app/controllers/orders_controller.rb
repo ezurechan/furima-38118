39eb60ca58340069f894fa007714dc66ec21283e
ezurechan <ezurem11+github@gmail.com>
2022-07-12 16:50:26 +0900

    挙動確認依頼後修正1

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -40,6 +40,6 @@ class OrdersController < ApplicationController
   end
 
   def contributor_confirmation
-    redirect_to root_path if current_user == @item.user
+    redirect_to root_path unless current_user == @item.user && @item.order.presence?
   end
 end
